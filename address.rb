bfb32860fb994aefc5d5bbe9b15f46291dc62ebe
Callum <git@nothx.pw>
2017-07-17 15:59:09 +0100

    Technical test address building

---
+++ address.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+sample_data = {
+    "subpremise": "Apt 6",
+    "house_number": "22",
+    "house_name": nil,
+    "street_line_1": "Bailey Rd",
+    "street_line_2": nil,
+    "town_or_city": "Sale",
+    "region": "Greater Manchester",
+    "postcode": "M33 1AX",
+}
+
+def build_address(hash)
+    address = []
+    address << [hash[:subpremise], hash[:house_name]].compact().join(", ")
+    address << [hash[:house_number], hash[:street_line_1]].compact().join(" ")
+    address << hash[:street_line_2] unless hash[:street_line_2].nil?
+    address << hash[:town_or_city]
+    address << hash[:region]
+    address << hash[:postcode]
+
+    return address.join(",\n")
+end
+
+puts build_address(sample_data)
